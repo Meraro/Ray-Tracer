966825b8d6c53ccd51abb07b6ebf9ad852a7cc48
Junyoung Choi <118741054+Meraro@users.noreply.github.com>
2026-03-20 18:53:45 +0900

    feat: add camera positioning

--- src/main.cc
+++ src/main.cc
@@ -11,28 +11,31 @@
 int main() {
     hittable_list world;
 
-    auto material_ground = 
-        make_shared<lambertian>(color(0.8, 0.8, 0));
-    auto material_center = 
-        make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = 
-        make_shared<metal>(color(0.8, 0.8, 0.8), 0.2);
-    auto material_right = 
-        make_shared<metal>(color(0.8, 0.6, 0.2), 0.8);
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left   = make_shared<dielectric>(1.50);
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
-    world.add(make_shared<sphere>(point3(0, -100.5, -1.0), 100, material_ground));
-    world.add(make_shared<sphere>(point3(0, 0, -1.2), 0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1, 0, -1), 0.5, material_left));
-    world.add(make_shared<sphere>(point3(1, 0, -1), 0.5, material_right));
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     double aspect_ratio = 16.0 / 9.0;
     int image_width = 800;
-    std::shared_ptr<const antialiasing> aa_strategy = std::make_shared<random_antialiasing>(50);
+    std::shared_ptr<const antialiasing> aa_strategy = std::make_shared<random_antialiasing>(10);
     
     camera cam(aspect_ratio, image_width);
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 400;
     cam.max_depth = 50;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(-2, 2, 1);
+    cam.lookat = point3(0, 0, -1);
+    cam.vup = vec3(0, 1, 0);
     cam.set_antialiasing(aa_strategy);
     // aa_strategy = std::make_shared<random_antialiasing>(50);
     // aa_strategy = std::make_shared<grid_antialiasing>(4);
