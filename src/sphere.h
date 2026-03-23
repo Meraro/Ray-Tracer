b642fc541d827af55b6da3a6f2ca90ef5cf39322
Junyoung Choi <118741054+Meraro@users.noreply.github.com>
2026-03-23 16:59:35 +0900

    feat: add motion blur

--- src/sphere.h
+++ src/sphere.h
@@ -2,18 +2,23 @@
 #define SPHERE_H
 
 #include "hittable.h"
+#include "ray.h"
 #include <memory>
 
 
 class sphere : public hittable {
 public:
-    sphere(const point3& center, double radius, std::shared_ptr<material> mat) :
-            center(center), radius(std::fmax(0, radius)), mat(mat) {
-            // TODO: Initialize the material pointer 'mat'.
-            };
+    // Stationary Sphere
+    sphere(const point3& static_center, double radius, shared_ptr<material> mat)
+        : center(static_center, vec3(0,0,0)), radius(std::fmax(0,radius)), mat(mat) {}
+    
+    // Moving Sphere
+    sphere(const point3& center1, const point3& center2, double radius, shared_ptr<material> mat)
+        : center(center1, center2 - center1), radius(std::fmax(0,radius)), mat(mat) {}
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-        vec3 oc = center - r.origin();
+        point3 current_center = center.at(r.time());
+        vec3 oc = current_center - r.origin();
         auto a = r.direction().length_squared();
         auto h = dot(r.direction(), oc);
         auto c = oc.length_squared() - radius * radius;
@@ -32,7 +37,7 @@ public:
 
         rec.t = root;
         rec.p = r.at(root);
-        vec3 outward_normal = (rec.p - center) / radius;
+        vec3 outward_normal = (rec.p - current_center) / radius;
         rec.set_face_normal(r, outward_normal);
         rec.mat = mat;
 
@@ -41,7 +46,7 @@ public:
 
 
 private:
-    point3 center;
+    ray center;
     double radius;
     std::shared_ptr<material> mat;
 };
